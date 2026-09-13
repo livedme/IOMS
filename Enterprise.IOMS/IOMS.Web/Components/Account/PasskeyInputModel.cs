adf91992eb3c98f9c47d6becb8767048d79fb097
Aminul Haque <aminul.edu.cse@gmail.com>
2026-09-13 16:44:46 +0600

    latest

--- Enterprise.IOMS/IOMS.Web/Components/Account/PasskeyInputModel.cs
+++
@@ -1,8 +0,0 @@
-namespace IOMS.Web.Components.Account
-{
-    public class PasskeyInputModel
-    {
-        public string? CredentialJson { get; set; }
-        public string? Error { get; set; }
-    }
-}
--- Enterprise.IOMS/IOMS.Web/Components/Account/PasskeyOperation.cs
+++
@@ -1,8 +0,0 @@
-namespace IOMS.Web.Components.Account
-{
-    public enum PasskeyOperation
-    {
-        Create = 0,
-        Request = 1,
-    }
-}
